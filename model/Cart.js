456c4fbf47a495d5a1323a91199aa6024c86cc67
Deekshitha32 <deekshithachirumamilla@gmail.com>
2024-06-13 00:21:19 +0530

    initial Commit

---
+++ model/Cart.js
@@ -0,0 +1,12 @@
+
+const mongoose = require('mongoose')
+
+const cartSchema = new mongoose.Schema({
+    p_id: Number,
+    p_img: String,
+    p_cost: Number,
+    p_qty: Number,
+    u_name: String
+})
+
+module.exports = mongoose.model("Cart", cartSchema)
